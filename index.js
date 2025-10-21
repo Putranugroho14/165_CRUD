cd57ddf76e040b89c97e5348f72ebaba54aa8d81
Putranugroho14 <putranugroho61@gmail.com>
2025-10-21 14:17:59 +0700

    membuat app listen

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+}); 
